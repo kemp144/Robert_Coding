fc156e591f4b601ae93522748ba1a706033aa21b
Robert Engel <robert.engel@misterspex.de>
2024-03-14 11:35:57 +0100

    First commit - hibernation works

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Minutes = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_Countdown = new System.Windows.Forms.Label();
+            this.richTextBox_Output = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hibernate PC in minutes";
+            // 
+            // textBox_Minutes
+            // 
+            this.textBox_Minutes.Location = new System.Drawing.Point(206, 67);
+            this.textBox_Minutes.Name = "textBox_Minutes";
+            this.textBox_Minutes.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Minutes.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_Countdown
+            // 
+            this.label_Countdown.AutoSize = true;
+            this.label_Countdown.Location = new System.Drawing.Point(526, 73);
+            this.label_Countdown.Name = "label_Countdown";
+            this.label_Countdown.Size = new System.Drawing.Size(115, 16);
+            this.label_Countdown.TabIndex = 3;
+            this.label_Countdown.Text = "Countdown_Timer";
+            // 
+            // richTextBox_Output
+            // 
+            this.richTextBox_Output.Location = new System.Drawing.Point(38, 164);
+            this.richTextBox_Output.Name = "richTextBox_Output";
+            this.richTextBox_Output.Size = new System.Drawing.Size(701, 261);
+            this.richTextBox_Output.TabIndex = 4;
+            this.richTextBox_Output.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(513, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PC Will Hibernate in:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox_Output);
+            this.Controls.Add(this.label_Countdown);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Minutes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Minutes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_Countdown;
+        private System.Windows.Forms.RichTextBox richTextBox_Output;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer countdownTimer = new Timer();
+        private TimeSpan timeRemaining;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the timer to tick every second
+            countdownTimer.Interval = 1000;
+            // Hook up the Tick event
+            countdownTimer.Tick += new EventHandler(countdownTimer_Tick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Try to parse the minutes entered by the user
+            if (int.TryParse(textBox_Minutes.Text, out int minutes))
+            {
+                // Initialize the remaining time span
+                timeRemaining = TimeSpan.FromMinutes(minutes);
+                // Update the label
+                label_Countdown.Text = timeRemaining.ToString(@"mm\:ss");
+                // Enable the timer
+                countdownTimer.Enabled = true;
+                // Append diagnostic message to RichTextBox
+                AppendToOutput("Timer enabled. Remaining time: " + timeRemaining);
+            }
+            else
+            {
+                // Show a message if the input is not valid
+                MessageBox.Show("Please enter a valid number of minutes.");
+            }
+        }
+
+        private void countdownTimer_Tick(object sender, EventArgs e)
+        {
+            // Decrease the remaining time
+            timeRemaining = timeRemaining.Subtract(TimeSpan.FromSeconds(1));
+            // Update the label
+            label_Countdown.Text = timeRemaining.ToString(@"mm\:ss");
+            // Append diagnostic message to RichTextBox
+            AppendToOutput("Tick event. Remaining time: " + timeRemaining);
+
+            // Check if the countdown has finished
+            if (timeRemaining <= TimeSpan.Zero)
+            {
+                // Stop the timer
+                countdownTimer.Enabled = false;
+                // Optionally notify the user
+                MessageBox.Show("Countdown finished!");
+                // Append diagnostic message to RichTextBox
+                AppendToOutput("Countdown finished.");
+                // Hibernate the computer
+                Process.Start("shutdown", "/h");
+            }
+        }
+
+        private void AppendToOutput(string message)
+        {
+            // Append the message to the RichTextBox
+            richTextBox_Output.AppendText(message + Environment.NewLine);
+        }
+    }
+}
