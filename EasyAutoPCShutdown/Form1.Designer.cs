516d991698e11d5a9a23d624b5c5c97fcb401d43
Robert Engel <robert.engel@misterspex.de>
2024-03-15 16:21:03 +0100

    Commit

---
+++ EasyAutoPCShutdown/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿using System.Windows.Forms;
+
+namespace EasyAutoPCShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Minutes = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_Countdown = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ShowWarning = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hibernate PC in minutes";
+            // 
+            // textBox_Minutes
+            // 
+            this.textBox_Minutes.Location = new System.Drawing.Point(215, 94);
+            this.textBox_Minutes.Name = "textBox_Minutes";
+            this.textBox_Minutes.Size = new System.Drawing.Size(82, 22);
+            this.textBox_Minutes.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_Countdown
+            // 
+            this.label_Countdown.AutoSize = true;
+            this.label_Countdown.Location = new System.Drawing.Point(593, 93);
+            this.label_Countdown.Name = "label_Countdown";
+            this.label_Countdown.Size = new System.Drawing.Size(0, 16);
+            this.label_Countdown.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(459, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PC Will Hibernate in:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(311, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Copyright (c) 2024 Robert Engel. All rights reserved.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(277, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Easy Auto PC Power Switch";
+            // 
+            // ShowWarning
+            // 
+            this.ShowWarning.AutoSize = true;
+            this.ShowWarning.Checked = true;
+            this.ShowWarning.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ShowWarning.Location = new System.Drawing.Point(51, 139);
+            this.ShowWarning.Name = "ShowWarning";
+            this.ShowWarning.Size = new System.Drawing.Size(167, 20);
+            this.ShowWarning.TabIndex = 10;
+            this.ShowWarning.Text = "Show 1 minute Warning";
+            this.ShowWarning.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(558, 265);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBoxDonate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(487, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(269, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Product is free to use. \r\nIf you would like, you can donate via PayPal.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shut Down",
+            "Hibernate"});
+            this.comboBox1.Location = new System.Drawing.Point(176, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Select Mode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 329);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ShowWarning);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_Countdown);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Minutes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Easy Auto PC Power Switch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Minutes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_Countdown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox ShowWarning;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private ComboBox comboBox1;
+        private Label label6;
+    }
+}
+
---
+++ EasyAutoPCShutdown/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+
+namespace EasyAutoPCShutdown
+{
+    public partial class Form1 : Form
+    {
+        private Timer countdownTimer = new Timer();
+        private TimeSpan timeRemaining;
+        private char selectedAction; // Variable to store the selected action
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the timer to tick every second
+            countdownTimer.Interval = 1000;
+            // Hook up the Tick event
+            countdownTimer.Tick += new EventHandler(countdownTimer_Tick);
+
+            selectedAction = EasyAutoPCShutdown.Properties.Settings.Default.LastSelectedAction;
+
+            // If no action has been selected, default to 's' (Shut Down)
+            if (selectedAction != 's' && selectedAction != 'h')
+            {
+                selectedAction = 's';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Try to parse the minutes entered by the user
+            if (int.TryParse(textBox_Minutes.Text, out int minutes))
+            {
+                // Initialize the remaining time span
+                timeRemaining = TimeSpan.FromMinutes(minutes);
+                // Update the label
+                label_Countdown.Text = timeRemaining.ToString(@"mm\:ss");
+                // Enable the timer
+                countdownTimer.Enabled = true;
+                // Save the last minutes value to application settings
+                EasyAutoPCShutdown.Properties.Settings.Default.LastMinutesValue = minutes;
+                EasyAutoPCShutdown.Properties.Settings.Default.Save();
+            }
+            else
+            {
+                // Show a message if the input is not valid
+                MessageBox.Show("Please enter a valid number of minutes.");
+            }
+        }
+
+        private void countdownTimer_Tick(object sender, EventArgs e)
+        {
+            // Decrease the remaining time
+            timeRemaining = timeRemaining.Subtract(TimeSpan.FromSeconds(1));
+            // Update the label
+            label_Countdown.Text = timeRemaining.ToString(@"mm\:ss");
+
+            if (timeRemaining == TimeSpan.FromMinutes(1) && ShowWarning.Checked)
+            {
+                // Ensure the form is visible and on top
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.WindowState = FormWindowState.Normal; // Restore the window
+                }
+                this.BringToFront(); // Bring the form to the front
+                this.TopMost = true; // Make the form topmost momentarily
+
+                // Display a warning message to the user
+                DialogResult result = MessageBox.Show("System will hibernate in 1 minute. Do you want to cancel hibernation?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                this.TopMost = false; // Revert the topmost status after the dialog is closed
+
+                if (result == DialogResult.Yes)
+                {
+                    // Cancel hibernation
+                    btnCancel_Click(sender, e);
+                }
+            }
+
+            // Check if the countdown has finished
+            if (timeRemaining <= TimeSpan.Zero)
+            {
+                // Stop the timer
+                countdownTimer.Enabled = false;
+                // Perform the shutdown or hibernation
+                TakeAction(selectedAction);
+                // Exit the app
+                Application.Exit();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Stop the timer
+            countdownTimer.Enabled = false;
+            // Optionally notify the user
+            MessageBox.Show(this, "Task canceled!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Reset the timer if cancellation occurs within the 5-minute window
+            if (timeRemaining <= TimeSpan.FromMinutes(5) && timeRemaining > TimeSpan.Zero)
+            {
+                int minutes;
+                if (int.TryParse(textBox_Minutes.Text, out minutes))
+                {
+                    // Initialize the remaining time span
+                    timeRemaining = TimeSpan.FromMinutes(minutes);
+                    // Update the label
+                    label_Countdown.Text = timeRemaining.ToString(@"mm\:ss");
+                }
+            }
+        }
+
+        private void TakeAction(char action)
+        {
+            string command = (action == 's') ? "/s /t 0" : "/h";
+            Process.Start("shutdown", command);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load the last minutes value from application settings and set it in the textbox
+            textBox_Minutes.Text =  EasyAutoPCShutdown.Properties.Settings.Default.LastMinutesValue.ToString();
+
+            // Load the last selected action from application settings and set it in the ComboBox
+            switch (selectedAction)
+            {
+                case 's':
+                    comboBox1.SelectedItem = "Shut Down";
+                    break;
+                case 'h':
+                    comboBox1.SelectedItem = "Hibernate";
+                    break;
+                default:
+                    break;
+            }
+
+            // Center the form on the screen
+            CenterFormOnScreen();
+        }
+
+        private void CenterFormOnScreen()
+        {
+            // Calculate the center position of the screen
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            int formWidth = this.Width;
+            int formHeight = this.Height;
+            int centerX = (screenWidth - formWidth) / 2;
+            int centerY = (screenHeight - formHeight) / 2;
+
+            // Set the form's location to the center position
+            this.Location = new System.Drawing.Point(centerX, centerY);
+        }
+
+        private void PictureBoxDonate_Click(object sender, EventArgs e)
+        {
+            // Open the PayPal donation link in the default web browser
+            Process.Start($"https://www.paypal.com/paypalme/engelrobert");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedOption = comboBox1.SelectedItem.ToString();
+
+            // Store the selected action
+            switch (selectedOption)
+            {
+                case "Shut Down":
+                    selectedAction = 's'; // Shutdown
+                    break;
+                case "Hibernate":
+                    selectedAction = 'h'; // Hibernate
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Override the OnFormClosing event to save settings before closing the form
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            // Save the last selected action to application settings
+            EasyAutoPCShutdown.Properties.Settings.Default.LastSelectedAction = selectedAction;
+            EasyAutoPCShutdown.Properties.Settings.Default.Save();
+        }
+
+    }
+}
